daf0def3f87d4e6c9bd95302a03795dde8ef4aa8
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-06-13 17:16:56 +0100

    day_02: NavBar - Done

--- day_02/src/components/NavBar/NavBar.jsx
+++ day_02/src/components/NavBar/NavBar.jsx
@@ -1,14 +1,52 @@
-import React from "react";
-import { Menu } from "react-feather";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { Menu, X } from "react-feather";
+
+const variants = {
+  in: { y: "0", opacity: 1 },
+  out: { y: "-70vh", opacity: 0 },
+};
 
 const NavBar = () => {
+  const [slide, setSlide] = useState(false);
+  const animate = () => {
+    setSlide((prev) => !prev);
+  };
   return (
     <nav>
-      <h2 className="logo">Lopez Rivera</h2>
-      <div className="cta">
-        <p>Menu</p>
-        <Menu color="#1A1A1A" size={30} />
+      <a href="/">
+        <h2 className="nav__logo">
+          LOPEZ RIVERA<sup>TM</sup>
+        </h2>
+      </a>
+      <div className="nav__icon" onClick={animate}>
+        <p>MENU</p>
+        <Menu color="#000000" size={20} />
       </div>
+      <motion.div
+        variants={variants}
+        animate={slide ? "in" : "out"}
+        className="nav__slider"
+      >
+        <div className="nav__icon" onClick={animate}>
+          <p>CLOSE</p>
+          <X color="#FFFFFF" size={20} />
+        </div>
+        <ul>
+          <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            ABOUT
+          </motion.li>
+          <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            PROJECTS
+          </motion.li>
+          <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            NEWS
+          </motion.li>
+          <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            CONTRIBUTORS
+          </motion.li>
+        </ul>
+      </motion.div>
     </nav>
   );
 };
